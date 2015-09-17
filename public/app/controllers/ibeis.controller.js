5c645d50931fe292987884348daea6fb57dbac15
Jon Wrona <jonwrona@gmail.com>
2015-09-17 15:15:24 -0400

    changed workplace dropdown to radio buttons w/ dropdowns enhancement

--- public/app/controllers/ibeis.controller.js
+++ public/app/controllers/ibeis.controller.js
@@ -4,9 +4,16 @@ angular.module('ibeis.controller', [])
 		{ 'name': 'All Images', 'href': '/' }
 	];
 
+	$scope.workplaces = [
+		{ 'name': 'Workplace 1', 'id': 1, 'href': '/workplace-1' },
+		{ 'name': 'Workplace 2', 'id': 2, 'href': '/workplace-2' },
+		{ 'name': 'Workplace 3', 'id': 3, 'href': '/workplace-3' }
+	];
+
 	$scope.collections = [
 		{ 'name': 'Collection 1', 'href': '/collection-1' },
 		{ 'name': 'Collection 2', 'href': '/collection-2' },
 		{ 'name': 'Collection 3', 'href': '/collection-3' }
 	];
+
 }]);
