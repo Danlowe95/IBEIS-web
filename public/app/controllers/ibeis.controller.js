474106a501bfde4e11a76e68f23588601a42b67e
Jon Wrona <jonwrona@gmail.com>
2015-09-21 14:16:47 -0400

    changed theme, added basic functionality for demoing

--- public/app/controllers/ibeis.controller.js
+++ public/app/controllers/ibeis.controller.js
@@ -2,21 +2,19 @@ angular.module('ibeis.controller', [])
 .controller('ibeis-controller', ['$scope', function($scope) {
 
 	$scope.workplaces = [
-		{ 'name': 'Workplace 1', 'id': 1, 'href': '/workplace-1' },
-		{ 'name': 'Workplace 2', 'id': 2, 'href': '/workplace-2' },
-		{ 'name': 'Super long workspace name to test this thing that is happening', 'id': 3, 'href': '/workplace-3' }
+		{ 'name': 'Workplace 1', 'id': 1, 'href': '' },
+		{ 'name': 'Workplace 2', 'id': 2, 'href': '' },
+		{ 'name': 'Super long workspace name to test this thing that is happening', 'id': 3, 'href': '' }
 	];
 
-	$scope.collections = [
-		{ 'name': 'Collection 1', 'href': '/collection-1' },
-		{ 'name': 'Collection 2', 'href': '/collection-2' },
-		{ 'name': 'Collection 3', 'href': '/collection-3' },
-		{ 'name': 'Collection 4', 'href': '/collection-4' },
-		{ 'name': 'Collection 5', 'href': '/collection-5' },
-		{ 'name': 'Collection 6', 'href': '/collection-6' },
-		{ 'name': 'Collection 7', 'href': '/collection-7' },
-		{ 'name': 'Collection 8', 'href': '/collection-8' },
-		{ 'name': 'Collection 9', 'href': '/collection-9' }
-	];
+	$scope.createWorkplace = function() {
+		$scope.workplaces.push({ 'name': 'New Workplace', 'id': Math.floor((Math.random() * 100) + 1), 'href': '' });
+	};
+
+	$scope.collections = [];
+
+	$scope.addCollection = function() {
+		$scope.collections.push({ 'name': 'NewCollection', 'href': '' });
+	};
 
 }]);
---
+++ public/assets/docs/test.md
@@ -0,0 +1,16 @@
+TEST DOC
+===
+
+This is a test
+---
+
+- - -
+
+1. This
+2. Is
+3. A
+4. Test
+
+Some text.
+
+A new paragraph.
