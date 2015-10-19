728b63f6daa59ae162d5c79daca28a5a1dbf209e
Jon Wrona <jonwrona@gmail.com>
2015-10-19 17:27:28 -0400

    updated test items

--- public/app/controllers/workspace.controller.js
+++ public/app/controllers/workspace.controller.js
@@ -1,13 +1,46 @@
 angular.module('workspace.controller', [])
-    .controller('workspace-controller', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
+    .controller('workspace-controller', ['$scope', '$mdSidenav', '$mdToast', function($scope, $mdSidenav, $mdToast) {
+        $scope.testItems = Array.apply(null, {length: 100}).map(Number.call, Number);
         $scope.toggleSidenav = function(menuId) {
             $mdSidenav(menuId).toggle();
         };
-        $scope.isOpenRight = function() {
-            return $mdSidenav('right').isOpen();
+
+
+        var last = {
+            bottom: false,
+            top: true,
+            left: false,
+            right: true
         };
-
-
+        $scope.toastPosition = angular.extend({}, last);
+        $scope.getToastPosition = function() {
+            sanitizePosition();
+            return Object.keys($scope.toastPosition)
+                .filter(function(pos) {
+                    return $scope.toastPosition[pos];
+                })
+                .join(' ');
+        };
+        function sanitizePosition() {
+            var current = $scope.toastPosition;
+            if (current.bottom && last.top) current.top = false;
+            if (current.top && last.bottom) current.bottom = false;
+            if (current.right && last.left) current.left = false;
+            if (current.left && last.right) current.right = false;
+            last = angular.extend({}, current);
+        }
+        $scope.undoFilter = function() {
+            var toast = $mdToast.simple()
+                .content('You undid your last filter!')
+                .action('REDO')
+                .highlightAction(false)
+                .position($scope.getToastPosition());
+            $mdToast.show(toast).then(function(response) {
+                if (response == 'redo') {
+                    alert('You redid the filter.');
+                }
+            });
+        };
     }])
     .controller('RightCtrl', function($scope, $timeout, $mdSidenav, $log) {
         $scope.close = function() {
