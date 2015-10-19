bd62e91c3c7e013e76699a6decdba5b5f3939c49
jon <jonwrona@gmail.com>
2015-10-19 19:29:54 -0400

    added table and map, moved to includes, map broken

--- public/app/ibeis.js
+++ public/app/ibeis.js
@@ -6,9 +6,11 @@ angular.module('ibeis', [
         // imports
         'ngMaterial', // angular material
         'yaru22.md', // markdown display (for help maybe)
-        'ncy-angular-breadcrumb' // breadcrumbs made easy
+        'ncy-angular-breadcrumb', // breadcrumbs made easy
+        'uiGmapgoogle-maps' // angular google maps
     ])
+    // setup theme
     .config(function($mdThemingProvider) {
         $mdThemingProvider.theme('default')
             .primaryPalette('green');
-    });;
+    });
