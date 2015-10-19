5ad797c774200132115faefbe9c33f2d8114268e
Jon Wrona <jonwrona@gmail.com>
2015-10-19 17:02:05 -0400

    added grid of thumbnails

--- public/app/ibeis.js
+++ public/app/ibeis.js
@@ -1,10 +1,14 @@
 angular.module('ibeis', [
-	// routes
-    'ibeis.routes',
-    // controllers
-    'workspace.controller',
-    // imports
-    'ngMaterial', // angular material
-    'yaru22.md', // markdown display (for help maybe)
-    'ncy-angular-breadcrumb' // breadcrumbs made easy
-]);
+        // routes
+        'ibeis.routes',
+        // controllers
+        'workspace.controller',
+        // imports
+        'ngMaterial', // angular material
+        'yaru22.md', // markdown display (for help maybe)
+        'ncy-angular-breadcrumb' // breadcrumbs made easy
+    ])
+    .config(function($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('green');
+    });;
