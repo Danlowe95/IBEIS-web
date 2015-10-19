5a1da83f91ec30a2b154ba2dc4a47923599dbaee
Danlowe95 <danlowe95@gmail.com>
2015-10-19 15:55:48 -0400

    filter stff

--- public/app/controllers/workspace.controller.js
+++ public/app/controllers/workspace.controller.js
@@ -3,4 +3,19 @@ angular.module('workspace.controller', [])
         $scope.toggleSidenav = function(menuId) {
             $mdSidenav(menuId).toggle();
         };
-    }]);
+        $scope.isOpenRight = function() {
+            return $mdSidenav('right').isOpen();
+        };
+
+
+    }])
+    .controller('RightCtrl', function($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function() {
+            $mdSidenav('right').close();
+        };
+        $scope.save = function() {
+            //save information required
+            $mdSidenav('right').close();
+        }
+
+    });
