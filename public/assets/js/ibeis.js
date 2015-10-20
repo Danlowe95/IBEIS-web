dedc2b8181fe451a5b62364517de5793bd292d4e
Jon Wrona <jonwrona@gmail.com>
2015-10-20 13:44:16 -0400

    resizing map not working still

--- public/assets/js/ibeis.js
+++ public/assets/js/ibeis.js
@@ -1,11 +1,16 @@
-$(window).load(resizeMap);
+$(window).load(function() {
+    alert("window load occurred!");
 
-$(window).resize(function() {
+    $(window).resize(function() {
+        resizeMap();
+    });
+
+    var resizeMap = function() {
+        console.log('resizing');
+        var total = $(window).height();
+        var toolbar = $("#toolbar").height();
+        $(".angular-google-map-container").css('height', total - toolbar);
+    };
+
     resizeMap();
 });
-
-var resizeMap = function() {
-	var total = $(window).height();
-	var toolbar = $("#toolbar").height();
-	$(".angular-google-map-container").css('height', total - toolbar);
-}
