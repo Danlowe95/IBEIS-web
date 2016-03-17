92b1bcf422658001d925785bc7558799c219d639
Jon Wrona <jonwrona@gmail.com>
2016-03-17 14:49:37 -0400

    upload work, still broke

--- public/app/controllers/upload.controller.js
+++ public/app/controllers/upload.controller.js
@@ -26,37 +26,39 @@ var upload = angular.module('upload', [])
 
       $scope.uploadedImages = [];
       $scope.upload = function(element) {
-      	console.log(element.files);
-      	var justFiles = $.map(element.files, function(val, key) {
-          return val;
-        }, true);
-  			console.log(justFiles);
-  			var newFiles = _.difference(justFiles, $scope.uploadedImages);
-  			for (i in newFiles) {
-  				var params = {
-            Key: newFiles[i].name,
-            ContentType: newFiles[i].type,
-            Body: newFiles[i]
-          }
-          $scope.uploadedImages.push(newFiles[i]);
-
-          AWS.config.update({
-            accessKeyId: 'AKIAJFVBLOTSKZ5554EA',
-            secretAccessKeyId: 'MWaPEpplIlHNeZspL6krTKh/muAa3l6rru5fIiMn'
-          });
-          AWS.config.region = 'us-west-2';
-          // console.log(AWS.config);
-          var uploader = new AWS.S3({ params: { Bucket: 'flukebook-dev-upload-tmp' } });
-          uploader.upload(params, function(err, data) {
-            if (err) {
-              console.error(err);
-            } else {
-              console.log("COMPLETED UPLOAD");
+        $scope.$apply(function(scope) {
+          console.log(element.files);
+          var justFiles = $.map(element.files, function(val, key) {
+            return val;
+          }, true);
+          console.log(justFiles);
+          var newFiles = _.difference(justFiles, scope.uploadedImages);
+          for (i in newFiles) {
+            var params = {
+              Key: newFiles[i].name,
+              ContentType: newFiles[i].type,
+              Body: newFiles[i]
             }
-          }).on('httpUploadProgress', function(progress) {
-            console.log(Math.round(progress.loaded / progress.total * 100));
-          });
-  			}
+            scope.uploadedImages.push(newFiles[i]);
+
+            AWS.config.update({
+              accessKeyId: 'AKIAJFVBLOTSKZ5554EA',
+              secretAccessKeyId: 'MWaPEpplIlHNeZspL6krTKh/muAa3l6rru5fIiMn'
+            });
+            AWS.config.region = 'us-west-2';
+            // console.log(AWS.config);
+            var uploader = new AWS.S3({ params: { Bucket: 'flukebook-dev-upload-tmp' } });
+            uploader.upload(params, function(err, data) {
+              if (err) {
+                console.error(err);
+              } else {
+                console.log("COMPLETED UPLOAD");
+              }
+            }).on('httpUploadProgress', function(progress) {
+              console.log(Math.round(progress.loaded / progress.total * 100));
+            });
+          }
+        });
       };
 
     }
